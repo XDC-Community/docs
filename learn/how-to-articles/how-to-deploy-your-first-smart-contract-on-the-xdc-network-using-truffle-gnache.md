bf3d1f603167904b019032b9dd2a288968024ef4
Jon McBee <jon@xdc.org>
2022-09-15 15:01:18 -0400

    refactoring layout

--- learn/how-to-articles/how-to-deploy-your-first-smart-contract-on-the-xdc-network-using-truffle-gnache.md
+++
@@ -1,6 +0,0 @@
----
-description: Hello world
----
-
-# How To deploy your first smart contract on the XDC Network using Truffle
-
--- learn/how-to-articles/smart-contracts/truffle/how-to-deploy-your-first-smart-contract-with-truffle/Pizza/migrations/1_pizza_migration.js
+++
@@ -1,8 +0,0 @@
-const Pizza = artifacts.require("Pizza");
-
-// Constructor variables can be declared here
-const SLICES = 8;  
-
-module.exports = function (deployer) {
-    deployer.deploy(Pizza, [SLICES]);
-}
